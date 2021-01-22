323a272f523817d9717c8d36f287436d14c3a3a8
Gina A <ginadzani@gmail.com>
2021-01-22 11:05:54 +0100

    Fix eslint error and HTML code error (change button to div)

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ console.log(recipesArray);
 const create = (elm, attributes) => {
 	const element = document.createElement(elm);
 	for (let key in attributes) {
-    	element.setAttribute(key, attributes[key])
-  	}
+		element.setAttribute(key, attributes[key])
+	}
 	return element;
 }
 
